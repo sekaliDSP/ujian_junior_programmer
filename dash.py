353ac655211a4aa2ee18ae0c6c1e127e7a3f2fdb
sekaliDSP <57188672+sekaliDSP@users.noreply.github.com>
2022-02-24 14:02:42 +0700

    push

--- dash.py
+++ dash.py
@@ -1,1 +1,25 @@
+def dash():
+    number = str(input("masukkan angka yang ingin dihitung!"))
+    keluaran = []
+    angka = []
+    for i in (str(number)):
+        angka.append(int(i))
+    print(angka)
+
+    for i in range(0,len(angka)):
+        if angka[i]%2 == 0:
+            try:
+                if (angka[i+1])%2 == 0:
+                    keluaran.append(angka[i])
+                    keluaran.append("-")
+                else:
+                    keluaran.append(angka[i])
+            except Exception  as e:
+                keluaran.append(angka[len(angka)-1])
+        else:
+            keluaran.append(angka[i])
+    print(keluaran)
+    for i in keluaran:
+        print(i, end="")
+dash()
 
